4eee0ca6200ff6ab8a7e250f0ec90ba1e06aedfb
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2020-11-21 14:41:07 +0700

    Customize input"

--- src/App.js
+++ src/App.js
@@ -18,14 +18,22 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [songInfo, setSongInfo] = useState({
         currentTime: 0,
-        duration: 0
+        duration: 0,
+        animationPercentage: 0
     })
   const [libraryStatus, setLibraryStatus] = useState(false);
   // Func
   const timeUpdateHandler = (e) => {
       const current = e.target.currentTime;
       const duration = e.target.duration;
-      setSongInfo({...songInfo, currentTime: current, duration:duration})
+      // Calculate percentage
+      const roundedCurrent = Math.round(current);
+      const roundedDuration = Math.round(duration);
+      const roundedAnimationPercentage = Math.round((roundedCurrent / roundedDuration) * 100);
+      setSongInfo({...songInfo, 
+          currentTime: current, 
+          duration:duration, 
+          animationPercentage:roundedAnimationPercentage})
   }
   return (
     <div className="App">
